2db191d2f33d7e287873403a70797bea04b721cd
Angela Shen <angelayshen@gmail.com>
2023-06-19 15:17:33 -0400

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w') as file_name:
+        file_name.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a') as file_name:
+        file_name.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as file_name:
+        return file_name.read()
+
+write_file(file_name="myfile", file_content="hello world")
+append_file(file_name="myfile", append_content="this is cool")
+read_file(file_name="myfile")
